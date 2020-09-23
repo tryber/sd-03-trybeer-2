56438c0c523c6834cfb179bcdaf2c7f49b22bc22
ilan274 <ilan.herbach@gmail.com>
2020-09-23 16:36:09 -0300

    Port changed to 3001 as required

--- back-end/index.js
+++ back-end/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.listen('/login',);
 
-app.put('/verificar', async (req, res) => {
+app.post('/verificar', async (req, res) => {
   const { email } = req.body;
   console.log(email);
   const verifyRegistration = await verifyIfExists({ email });
@@ -23,8 +23,8 @@ app.put('/verificar', async (req, res) => {
 });
 
 app.post('/register', async (req, res) => {
-  const { name, email, password, role } = req.body;
-  const userObject = { name, email, password, role };
+  const { name, email, password, role } = req.body; // controller
+  const userObject = { name, email, password, role }; // ..
   if (!name || !email || !password || !role) {
     return res.status(400).json({ message: 'Envie nome, email, password e função (role)' });
   }
@@ -61,5 +61,5 @@ app.post('/login', verifyToken, (req, res) => {
   res.status(200).json({ message: 'ok', role: req.user.role });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Running on :${PORT}`));

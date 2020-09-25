1156fd1f8cb9707d4803ae9dff61946bd4d9217b
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-09-25 17:18:58 -0300

    problem with data-base  expect :E-mail already in database

--- front-end/src/Pages/Register/index.js
+++ front-end/src/Pages/Register/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import UserRegister from '../../Components/Register/UserRegister';
 
-const Register = () => (<div>
-    <UserRegister />
-  </div>
-);
+const Register = () => (<div><UserRegister /></div>);
 
 export default Register;

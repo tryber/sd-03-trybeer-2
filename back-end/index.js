5d5818a43159d32d8ea06f51c28606b110ca621a
DiegoRafaelAF <draf0314@gmail.com>
2020-10-01 16:39:32 -0300

    ...

--- back-end/index.js
+++ back-end/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const loginController = require('./controllers/loginController');
 const registerController = require('./controllers/registerController');
 const profileController = require('./controllers/profileController');
+const productsControler = require('./controllers/productsControler');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/images', express.static(path.join(__dirname, '/images')));
+app.get('/products', productsControler);
+
 app.post('/login', loginController);
 app.post('/register', registerController);
 app.post('/profile', profileController);
--- back-end/models/connection.js
+++ back-end/models/connection.js
@@ -1,17 +1,23 @@
 require('dotenv/config');
 const mysqlx = require('@mysql/xdevapi');
-
-const connection = () => mysqlx.getSession({
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  host: process.env.HOSTNAME,
-  port: 33060,
-  schema: 'Trybeer',
-})
-  .then((session) => session.getSchema('Trybeer'))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
-
+let schema;
+const connection = () =>
+  schema
+    ? Promise.resolve(schema)
+    : mysqlx
+        .getSession({
+          user: process.env.MYSQL_USER,
+          password: process.env.MYSQL_PASSWORD,
+          host: process.env.HOSTNAME,
+          port: 33060,
+          schema: 'Trybeer',
+        })
+        .then((session) => {
+          schema = session.getSchema('Trybeer');
+          return schema;
+        })
+        .catch((err) => {
+          console.error(err);
+          process.exit(1);
+        });
 module.exports = connection;

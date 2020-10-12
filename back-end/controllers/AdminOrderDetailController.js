eb483c52918f941b810c63fbf50d5b37d6b91062
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-10-12 11:11:49 -0300

    Erros Lint repaired

--- back-end/controllers/AdminOrderDetailController.js
+++ back-end/controllers/AdminOrderDetailController.js
@@ -4,7 +4,6 @@ const OrderModel = require('../models/AdminDetailModel');
 const controllerDetailsOrder = rescue(async (_req, res) => {
   const callModel = await OrderModel.AdminDetail();
   return res.status(200).json(callModel);
-  //Retorna como json//
 });
 
 module.exports = {

41b5acd95bcedd05466949fd83fb1ed63d8c232f
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-10-12 12:18:26 -0300

    Erros Lint repaired

--- back-end/services/verifyToken.js
+++ back-end/services/verifyToken.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
-const SECRET='2c48ad576fc39c27a37910eafc4c483faa0d81d40712596b950a49ddcebee4592dce95a6201376b2a7d65791d89999e86b366e345292d732eb7d9d3a6f75fce1';
-
+const SECRET = '2c48ad576fc39c27a37910eafc4c483faa0d81d40712596b950a49ddcebee4592dce95a6201376b2a7d65791d89999e86b366e345292d732eb7d9d3a6f75fce1';
 
 const verifyToken = (req, res, next) => {
   try {
